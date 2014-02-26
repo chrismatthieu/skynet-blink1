ed90392992de4ee962a241d1d3abc98d485061ce
Luis Montes <luis@iceddev.com>
2014-02-25 18:43:47 -0700

    initial push

---
+++ index.js
@@ -0,0 +1,72 @@
+
+var WeMo = require('wemo');
+
+function Plugin(messenger, options){
+  this.messenger = messenger;
+  this.options = options;
+  return this;
+}
+
+var optionsSchema = {
+  type: 'object',
+  properties: {
+    friendlyName: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+var messageSchema = {
+  type: 'object',
+  properties: {
+    on: {
+      type: 'boolean',
+      required: true
+    }
+  }
+};
+
+Plugin.prototype.onMessage = function(data, cb){
+  console.log('wemo data', data);
+  WeMo.Search(this.options.friendlyName, function(err, device) {
+
+    if(device){
+      var wemoSwitch = new WeMo(device.ip, device.port);
+      var binaryState = 0;
+      if(data.on){
+        binaryState = 1;
+      }
+      console.log(wemoSwitch);
+      wemoSwitch.setBinaryState(binaryState, function(serr, result) {
+        if (err){
+          console.error('error setting state', serr);
+        }else{
+          console.log(result);
+        }
+
+        if(cb){
+          cb(serr, result);
+        }
+
+      });
+    }else{
+      if(cb){
+        cb(err, null);
+      }
+    }
+  });
+
+};
+
+Plugin.prototype.destroy = function(){
+  //clean up
+  console.log('destroying.', this.options);
+};
+
+
+module.exports = {
+  Plugin: Plugin,
+  optionsSchema: optionsSchema,
+  messageSchema: messageSchema
+};
